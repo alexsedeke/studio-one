a6d01842e0aefc07e99c2f920f6783b8135f8050
Alexander Vu <alexander.vu.tuyet@gmail.com>
2021-03-23 23:10:08 +0100

    update subfolder

--- vite-app.config.ts
+++ vite-app.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://alexandervu.github.io/studio-one/',
+  base: 'studio-one/',
   build: {
     outDir: 'dist-app',
   }
